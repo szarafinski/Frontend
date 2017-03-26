d9d9d996c9a52f08ea4fdb711bc421f2c67c3c87
KrzysieK <krzysztof.szarafinski@tlen.pl>
2017-03-26 09:42:43 +0200

    zadanie 1  (ECMA 5) konstrukcja klas poprawiona na nowego typu

--- zad1/test.js
+++ zad1/test.js
@@ -1,49 +1,54 @@
-function Meal(json) {
-    var self = this;
-    self.publisher = json.publisher;
-    self.id = json.recipe_id;
-    self.title = json.title;
-    self.social_rank = json.social_rank;
-    self.image = json.image_url;
-    self.url = json.f2f_url;
+var Meal = (function () {
+    function Meal(json) {
+        this.publisher = json.publisher;
+        this.id = json.recipe_id;
+        this.title = json.title;
+        this.social_rank = json.social_rank;
+        this.image = json.image_url;
+        this.url = json.f2f_url;
+    };
 
-    self.toTableRow = function () {
+    Meal.prototype.toTableRow = function () {
         return '<tr><td>'
-            + self.id
+            + this.id
             + '</td><td>'
-            + self.publisher
+            + this.publisher
             + '</td><td>'
-            + '<a href=' + self.url + '>' + self.title + '</a>'
+            + '<a href=' + this.url + '>' + this.title + '</a>'
             + '</td><td>'
-            + self.social_rank
+            + this.social_rank
             + '</td><td>'
-            + makeImage(self.image)
+            + this.makeImage(this.image)
             + '</td></tr>'
-    }
+    };
 
-    function makeImage(url) {
+    Meal.prototype.makeImage = function (url) {
         return '<img class="img-thumbnail" width="304" height="236" src= ' + url + ">";
-    }
-}
-function ListOfMeals() {
-    var meals = [];
-    var self = this;
+    };
+    return Meal;
+})();
 
-    self.addMeal = function (json) {
-        meals.push(new Meal(json));
-    }
+var ListOfMeals = (function () {
+    function ListOfMeals() {
+        this.meals = [];
+    };
 
-    self.toTable = function () {
-        var table = '<table class="table table-striped table-bordered">';
-        table += generateTableHeader();
-        for (var i = 0; i < meals.length; i++) {
-            table += meals[i].toTableRow();
+    ListOfMeals.prototype.addMeal = function (json) {
+        this.meals.push(new Meal(json));
+    };
+
+    ListOfMeals.prototype.toTable = function () {
+        var self = this;
+        self.table = '<table class="table table-striped table-bordered">';
+        self.table += this.generateTableHeader();
+        for (var i = 0; i < this.meals.length; i++) {
+            self.table += this.meals[i].toTableRow();
         }
-        table += '</table>';
-        return table;
-    }
+        self.table += '</table>';
+        return self.table;
+    };
 
-    var generateTableHeader = function () {
+    ListOfMeals.prototype.generateTableHeader = function () {
         return '<tr>'
             + '<th>ID</th>'
             + '<th>Publisher</th>'
@@ -53,17 +58,18 @@ function ListOfMeals() {
             + '</tr>';
     }
 
-    self.clear = function () {
-        meals = [];
-    }
-}
+    ListOfMeals.prototype.clear = function () {
+        this.meals = [];
+    };
+    return ListOfMeals;
+})();
 
 function doJob(data) {
     console.log(data.recipes.length);
     var listOfMeals = new ListOfMeals();
-    for (var i = 0; i < data.recipes.length; i++){
+    for (var i = 0; i < data.recipes.length; i++) {
         listOfMeals.addMeal(data.recipes[i]);
-         if (i == data.recipes.length) {
+        if (i == data.recipes.length) {
             return false;
         }
     }
