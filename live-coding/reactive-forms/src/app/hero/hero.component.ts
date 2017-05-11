3d3579762551c6cb55482782c4ff6eaf34fbab11
KrzysieK <krzysztof.szarafinski@tlen.pl>
2017-05-11 17:40:49 +0200

    Nested FormGroups

--- live-coding/reactive-forms/src/app/hero/hero.component.ts
+++ live-coding/reactive-forms/src/app/hero/hero.component.ts
@@ -1,3 +1,4 @@
+import { states } from './../data-model';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
 
@@ -8,18 +9,31 @@ import { FormControl, FormGroup, AbstractControl, FormBuilder, Validators } from
 })
 export class HeroComponent implements OnInit {
   heroForm: FormGroup;
+  // to make <div> work value of GroupForm must be declared as TypeScript value <- 1
   color: AbstractControl;
 
-  constructor(fb: FormBuilder) {
+  states = states;
+  constructor(private fb: FormBuilder) {
     this.heroForm = fb.group({
+      //  GroupForm checks for valid input: name
       name: ['', Validators.required],
-      test: '',
+      // it is not nessesary to put validators
+      // it's nessesary to put empty value into variable
+      power: '',
+      // to make <div> work value of GroupForm must be declared as TypeScript value <- 2
       color: ['red', Validators.compose([
-              Validators.required,
-              Validators.minLength(5)]
-      )]
+        Validators.required,
+        Validators.minLength(5)]
+      )],
+      address: this.fb.group({
+        street: '',
+        city: '',
+        state: '',
+        zip: '',
+      }),
+      sidekick: ''
     });
-
+    // to make <div> work value of GroupForm must be declared as TypeScript value <- 3
     this.color = this.heroForm.controls['color'];
   }
 
